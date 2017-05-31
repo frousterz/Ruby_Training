ca75c9001795fb68088471e1ff66db736e46591d
Felipe Salas Casasola <frousterz@gmail.com>
2017-05-31 10:46:12 -0600

    Triangle function completed in file triangle.rb

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,15 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b && b == c
+    :equilateral
+  elsif a == b && b != c or
+      b == c && a != c or
+      a == c && a != b
+    :isosceles
+  elsif a != b && b != c && a != c
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
