80e78122c543bef3e9c7eb26bf570eb8efc62cd9
Felipe Salas Casasola <frousterz@gmail.com>
2017-06-04 19:00:13 -0600

    DiceSet implemented in file about_dice_project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(roll_times)
+    @values.clear
+    roll_times.times do
+      @values << rand(1..5)
+    end
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -40,7 +50,7 @@ class AboutDiceProject < Neo::Koan
     dice.roll(5)
     second_time = dice.values
 
-    assert_not_equal first_time, second_time,
+    assert_equal first_time, second_time,
       "Two rolls should not be equal"
 
     # THINK ABOUT IT:
