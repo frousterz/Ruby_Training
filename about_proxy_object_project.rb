590560aae513572a817a2b7e5f5bc2f062c255ec
Felipe Salas Casasola <frousterz@gmail.com>
2017-06-04 20:56:53 -0600

    Methods added to Proxy in file about_proxy_object_project

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,16 +13,30 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = Array.new
   end
 
   # WRITE CODE HERE
+  def method_missing(method_name, *arguments, &block)
+    @messages << method_name
+    @object.send method_name, *arguments, &block
+  end
+
+  def called?(method_name)
+    @messages.include? method_name
+  end
+
+  def number_of_times_called(method_name)
+    @messages.count(method_name)
+  end
 end
 
 # The proxy object should pass the following Koan:
-#
+
 class AboutProxyObjectProject < Neo::Koan
   def test_proxy_method_returns_wrapped_object
     # NOTE: The Television class is defined below
